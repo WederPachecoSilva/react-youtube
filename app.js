26a663b9fccc72d009135fee779cca8498ae1e58
WederPacheco <wederpacheco.s@gmail.com>
2017-07-11 16:10:17 -0300

    configuração pronta

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "dist")))
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname,"dist", "index.html"))
 })
 
 app.listen(3000, (req,res) => console.log("Rodando na porta 3000"));
